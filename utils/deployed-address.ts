35c3c03c3d14776fa98b91396cd7035ba8373820
thanhtienzz tan <tienthanhthai9@gmail.com>
2024-08-15 13:06:50 +0000

    Initial commit

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0x09d9D09fe5d361A55cb78A7682F3e9E55f91Ff3F'
-export const ProxyAdmin = '0x58dBCe6301413822E9B3F1c4cFc42D2c750CaAb2'
-export const SWTRImplementation = '0xa623de330d992eB698d56c271b2cc1e7e3D958B5'
+export const SWTRProxy = '0x5Bfd683fE31Cd11eDeB0baeA2FD6e6BDF19cc5dD'
+export const ProxyAdmin = '0xD9158126D87d9973208cAD56d1Cd7a325c2D74da'
+export const SWTRImplementation = '0xa99190716aff537E97678EDbAbe455444788D8aF'
     
